0f8ba947b6b6ca535c6729444217787d35e64208
Hanan Mohsin <129000701+Hanan501@users.noreply.github.com>
2024-03-05 19:10:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    let distance = Math.abs(blockNumber - 42);
+    return distance;
+  }
+  
+  function distanceFromHqInFeet(blockNumber) {
+    let distanceInFeet = distanceFromHqInBlocks(blockNumber) * 264;
+    return distanceInFeet;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    let distanceTravelled = Math.abs(destination - start) * 264;
+    return distanceTravelled;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400) {
+      return 0;
+    } else if (feet <= 2000) {
+      return (feet - 400) * 0.02;
+    } else if (feet <= 2500) {
+      return 25;
+    } else {
+      return "cannot travel that far";
+    }
+  }
